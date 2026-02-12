bc64a2e9db61e919794e5336c62ac606c9022442
Ahmed Hossam <ahmedhosssamps@gmail.com>
2026-02-12 05:28:30 +0200

    Finish first version of HTTPResponse and getRawHTTPResponse

--- main.go
+++ main.go
@@ -5,22 +5,30 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
+	"strconv"
 	"strings"
 )
 
-type HTTPReq struct {
-	method         string
-	path           string
-	httpVersion    string
-	host           string
-	userAgent      string
-	acceptEncoding string
+type HTTPRequest struct {
+	method      string
+	path        string
+	httpVersion string
+	headers     map[string]string
 }
 
-func parseHTTPRequest(req string) HTTPReq {
+type HTTPResponse struct {
+	httpVersion  string
+	statusCode   string
+	reasonPhrase string
+	headers      map[string]string
+	body         string
+}
+
+func parseHTTPRequest(req string) HTTPRequest {
 	lines := strings.Split(req, "\n")
 
-	var httpreq HTTPReq
+	var httpReq HTTPRequest
 
 	// Example of first line: "GET /api/data HTTP/1.1"
 	firstLine := strings.Split(lines[0], " ")
@@ -37,9 +45,11 @@ func parseHTTPRequest(req string) HTTPReq {
 		panic("INVALID METHOD: " + method)
 	}
 
-	httpreq.method = method
-	httpreq.path = path
-	httpreq.httpVersion = httpVersion
+	httpReq.method = method
+	httpReq.path = path[1:] // To remove the / in the beginning
+	httpReq.httpVersion = httpVersion
+
+	httpReq.headers = make(map[string]string)
 
 	for i := range len(lines) {
 		if i == 0 {
@@ -48,23 +58,106 @@ func parseHTTPRequest(req string) HTTPReq {
 
 		parts := strings.Split(lines[i], ": ")
 
-		if parts[0] == "User-Agent" {
-			httpreq.userAgent = parts[1]
+		if len(parts) < 2 {
+			continue
 		}
 
-		if parts[0] == "Host" {
-			httpreq.host = parts[1]
-		}
-
-		if parts[0] == "Accept-Encoding:" {
-			httpreq.acceptEncoding = parts[1]
-		}
+		httpReq.headers[parts[0]] = parts[1]
 	}
 
-	log.Printf("\n\n\nParsed HTTP Request:\n---\nMethod: %s\nPath: %s\nVersion: %s\nHost: %s\nUser-Agent: %s\nAccept-Encoding: %s\n---\n",
-		httpreq.method, httpreq.path, httpreq.httpVersion, httpreq.host, httpreq.userAgent, httpreq.acceptEncoding)
+	log.Printf(
+		"\n\n\nParsed HTTP Request:\n"+
+			"---\n"+
+			"Method: %s\n"+
+			"Path: %s\n"+
+			"Version: %s\n"+
+			"Host: %s\n"+
+			"User-Agent: %s\n"+
+			"Accept-Encoding: %s\n"+
+			"---\n",
+		httpReq.method,
+		httpReq.path,
+		httpReq.httpVersion,
+		httpReq.headers["Host"],
+		httpReq.headers["User-Agent"],
+		httpReq.headers["Accept-Encoding"],
+	)
 
-	return httpreq
+	return httpReq
+}
+
+func getHTTPResponse(httpReq HTTPRequest) HTTPResponse {
+	var httpRes HTTPResponse
+	httpRes.headers = make(map[string]string)
+
+	method := httpReq.method
+	path := httpReq.path
+
+	if method == "GET" {
+		// go get the content in the given path and put it in httpRes.body
+		data, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		httpRes.body = string(data)
+		httpRes.httpVersion = "HTTP/1.1"
+		httpRes.statusCode = "200"
+		httpRes.reasonPhrase = "OK"
+		httpRes.headers["Content-Type"] = "text/html"
+		httpRes.headers["Content-Length"] = strconv.Itoa(len(string(data)))
+		httpRes.headers["Connection"] = "keep-alive"
+	}
+
+	return httpRes
+}
+
+// HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 13\r\nConnection: keep-alive\r\n\r\nHello Vietnaaaaam\r\n\r\n
+
+func getRawHTTPResponse(httpRes HTTPResponse) string {
+	var rawResponse string
+
+	rawResponse += httpRes.httpVersion
+	rawResponse += " "
+	rawResponse += httpRes.statusCode
+	rawResponse += " "
+	rawResponse += httpRes.reasonPhrase
+	rawResponse += "\r\n"
+
+	for key, value := range httpRes.headers {
+		rawResponse += key + ": " + value + "\r\n"
+	}
+
+	rawResponse += "\r\n"
+	rawResponse += httpRes.body
+
+	fmt.Println(rawResponse)
+	return rawResponse
+}
+
+func handleTCPConnection(c net.Conn) {
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	tmp := make([]byte, 4096)
+	defer c.Close()
+	for {
+		_, err := c.Read(tmp)
+		req := string(tmp)
+		if strings.Contains(req, "HTTP") {
+			httpReq := parseHTTPRequest(string(tmp))
+			httpRes := getHTTPResponse(httpReq)
+			httpResRaw := getRawHTTPResponse(httpRes)
+			fmt.Println(httpResRaw)
+			response := fmt.Sprintf(httpResRaw)
+			c.Write([]byte(response))
+		}
+
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("read error:", err)
+			}
+			break
+		}
+	}
 }
 
 func main() {
@@ -82,29 +175,5 @@ func main() {
 			return
 		}
 		go handleTCPConnection(c)
-	}
-}
-
-func handleTCPConnection(c net.Conn) {
-	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-	packet := make([]byte, 4096)
-	tmp := make([]byte, 4096)
-	defer c.Close()
-	for {
-		_, err := c.Read(tmp)
-		req := string(tmp)
-		if strings.Contains(req, "HTTP") {
-			parseHTTPRequest(string(tmp))
-			response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 13\r\nConnection: keep-alive\r\n\r\nHello, World!\r\n\r\n")
-			c.Write([]byte(response))
-		}
-
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("read error:", err)
-			}
-			break
-		}
-		packet = append(packet, tmp...)
 	}
 }
